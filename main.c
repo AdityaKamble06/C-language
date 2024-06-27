eeb5417877f4899fadf7a2c414a1040ce3122649
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-06-27 10:49:39 +0530

    main.c
    C Program to Print an Integer (Entered by the User)

---
+++ main.c
@@ -0,0 +1,22 @@
+//C Program to Print an Integer (Entered by the User)//
+
+#include<stdio.h>
+
+int main()
+{
+ int number;
+ printf("Enter an integer: ");
+ 
+ //reads and stores input
+ scanf("%d",&number);
+ 
+ //displays output
+ printf("You entered:%d",number);
+ 
+ return 0;
+}
+
+
+//Output
+Enter an integer: 2
+You entered:2
