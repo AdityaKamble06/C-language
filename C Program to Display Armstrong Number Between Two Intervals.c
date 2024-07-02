e4f6d3a4a6a3af26b230208993ffe8fcdaca0bec
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-02 13:56:57 +0530

    Create C Program to Display Armstrong Number Between Two Intervals.c

---
+++ C Program to Display Armstrong Number Between Two Intervals.c
@@ -0,0 +1,61 @@
+//Armstrong Numbers Between Two Integers
+
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+ int low, high, number,originalNumber, rem, count = 0;
+ double result = 0.0;
+ printf("Enter two numbers(intervals): ");
+ scanf("%d %d", &low, &high);
+ printf("Armstrong numbers between %d and %d are: ", low, high);
+ 
+ //swap numbers if high < low
+ if(high < low)
+ {
+  high += low;
+  low = high - low;
+  high -= low;
+ }
+ 
+ //iterate number from (low + 1) to (high - 1)
+ //In each iteratin, check if number is Armstrong
+ 
+ for(number = low + 1; number < high; ++number)
+ {
+  originalNumber = number;
+  
+  //number of digid calculation
+  while(originalNumber != 0)
+  {
+   originalNumber /= 10;
+   ++count;
+  }
+  originalNumber = number;
+  
+  //result contains sum of nth power of individual digits
+  while(originalNumber != 0)
+  {
+   rem = originalNumber % 10;
+   result += pow(rem, count);
+   originalNumber /= 10;
+  }
+  
+  //check if number is equal to the sum of nth power of individual digits
+  if((int)result == number)
+  {
+   printf("%d ", number);
+  }
+  
+  //resetting the values
+  count = 0;
+  result = 0;
+ }
+ 
+ return 0;
+}
+
+//Output
+Enter two numbers(intervals): 200
+2000
+Armstrong numbers between 200 and 2000 are: 370 371 407 1634
