5774b24e8a07aa0daab5448b6e12930e7064fc5e
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-02 14:12:22 +0530

    Create C Program to Make a Simple Calculator Using switch...case.c

---
+++ C Program to Make a Simple Calculator Using switch...case.c
@@ -0,0 +1,47 @@
+//Simple Calculator using switch Statement
+
+#include<stdio.h>
+int main()
+{
+ char op;
+ double first, second;
+ printf("Enter an operator ( +, -, *, /): ");
+ scanf("%c", &op);
+ printf("Enter two operands: ");
+ scanf("%lf %lf", &first, &second);
+ 
+ switch(op)
+ {
+  case'+':
+  {
+   printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+  }
+  break;
+  case'-':
+  {
+   printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+  }
+  break;
+  case'*':
+  {
+   printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+  }
+  break;
+  case'/':
+  {
+   printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+  }
+  break;
+  //operator doesn't match any case constant
+  default:
+  {
+   printf("Error! operator is not correct.");
+  }
+ }
+ return 0;
+}
+
+//Output
+Enter an operator ( +, -, *, /): *
+Enter two operands: 2 3
+2.0 * 3.0 = 6.0
